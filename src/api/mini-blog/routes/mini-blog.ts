cb94a5d0f703f73135bfa440fe5986c1a4541fba
ALeafWolf <aleafwolf@outlook.com>
2023-07-28 12:58:45 -0400

    upgrade strapi to 4.12.0 and add /mini-blog

---
+++ src/api/mini-blog/routes/mini-blog.ts
@@ -0,0 +1,7 @@
+/**
+ * mini-blog router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::mini-blog.mini-blog');
---
+++ src/api/mini-blog/services/mini-blog.ts
@@ -0,0 +1,7 @@
+/**
+ * mini-blog service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::mini-blog.mini-blog');
